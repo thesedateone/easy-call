265f0cc5abbe4afb5fea64ebaa2d1c8bd569701a
Jacques Beukes <Jacques.Beukes@orionhealth.com>
2014-12-30 21:09:02 +1300

    really basic login and logout functionality

--- easyCall/urls.py
+++ easyCall/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'easyCall.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', include('pledges.urls')),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'},
+        name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login',
+        name='logout'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
