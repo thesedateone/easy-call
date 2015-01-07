d44e3fb3ac941e7f1331b1143094b9c506f4b85f
Jacques Beukes <Jacques.Beukes@orionhealth.com>
2015-01-07 16:01:17 +1300

    lists REST api only works for logged in users now.

--- easyCall/apps/lists/views.py
+++ easyCall/apps/lists/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import permissions
 from django.http import Http404
 
 from easyCall.apps.lists.models import ListType, CallResult
-from easyCall.apps.lists.serializers import ListTypeSerializer, CallResultSerializer
+from easyCall.apps.lists.serializers import ListTypeSerializer
+from easyCall.apps.lists.serializers import CallResultSerializer
 
 
 class ListTypeList(APIView):
@@ -11,6 +13,8 @@ class ListTypeList(APIView):
     """
     List all list types.
     """
+
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request, format=None):
         list_types = ListType.objects.all()
@@ -23,6 +27,8 @@ class CallResultList(APIView):
     """
     Retrieve details of a list type.
     """
+
+    permission_classes = (permissions.IsAuthenticated)
 
     def get_object(self, slug):
         try:
--- easyCall/urls.py
+++ easyCall/urls.py
@@ -13,6 +13,8 @@ urlpatterns = patterns(
         name='login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login',
         name='logout'),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
