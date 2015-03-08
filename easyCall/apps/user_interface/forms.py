dd68c89cb562e2cbf9cf42c6cbfe521c52751a86
Jacques Beukes <Jacques.Beukes@gmail.com>
2015-03-08 18:37:33 +1300

    Fixed bug where upload form was not updating when new types are added.

--- easyCall/apps/user_interface/forms.py
+++ easyCall/apps/user_interface/forms.py
@@ -2,10 +2,7 @@ from django import forms
 
 from easyCall.apps.lists.models import ListType
 
-LIST_TYPE_CHOICES = [(x.slug, x.display_name) for x in ListType.objects.all()]
-
 class UploadFileForm(forms.Form):
-    list_type = forms.ChoiceField(choices=LIST_TYPE_CHOICES, 
-                                  required=True)
+    list_type = forms.ModelChoiceField(queryset = ListType.objects.all(), required=True)
     import_only_headings = forms.BooleanField(required=False)
     file = forms.FileField(required=True)
