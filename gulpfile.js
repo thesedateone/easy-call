650540d4e8e24ee6563b8557d62132025e169052
Jacques Beukes <Jacques.Beukes@gmail.com>
2015-10-26 10:51:29 +1300

    Introduce new tools: * simplify package.json * move 3rd party library managment to bower * move js management to gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+var fs = require('fs')
+var gulp = require('gulp')
+var plumber = require('gulp-plumber')
+var concat = require('gulp-concat')
+
+// Application JS
+
+gulp.task('app_js', function() {
+  return gulp.src(['web/**/module.js', 'web/**/*.js'])
+    .pipe(plumber())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('assets/app/js'))
+})
+
+// Vendor JS
+
+var base = 'bower_components/**/'
+var vendor = [
+  base + 'dist/jquery.js',
+  base + 'dist/**/bootstrap.js',
+  base + 'lodash.js',
+  base + 'angular.js',
+  base + 'angular-route.js',
+  base + 'dist/restangular.js'
+]
+
+gulp.task('vendor_js', function() {
+  return gulp.src(vendor)
+    .pipe(plumber())
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('assets/vendor/js'))
+})
+
+// META Tasks
+
+gulp.task('js', ['app_js', 'vendor_js'])
