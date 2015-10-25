83f631d4710e84c120f76cabd0fe9935434e18ea
Jacques Beukes <Jacques.Beukes@gmail.com>
2015-10-26 11:01:45 +1300

    Update collect.js to keep things working while i figure out gulp

--- scripts/collect.js
+++ scripts/collect.js
@@ -1,11 +1,11 @@
 var fs = require('fs-extra');
 
-var vendorDir = './static/vendor';
+var vendorDir = './assets/vendor';
 var vendorJS = vendorDir + '/js';
 var vendorCSS = vendorDir + '/css';
 var vendorFonts = vendorDir + '/fonts';
 
-var appDir = './static/app';
+var appDir = './assets/app';
 var appImages = appDir + '/images';
 var appPartials = appDir + '/partials';
 var appJS = appDir + '/js';
@@ -14,8 +14,8 @@ var appJS = appDir + '/js';
 	Collect bootstrap & font awesome dist files
  */
 
-var bootstrapDir = './node_modules/bootstrap/dist';
-var fontAwesomeDir = './node_modules/font-awesome/';
+var bootstrapDir = './bower_components/bootstrap/dist';
+var fontAwesomeDir = './bower_components/font-awesome/';
 
 var bootstrapCSS = bootstrapDir + '/css';
 fs.copySync(bootstrapCSS + '/bootstrap.min.css', vendorCSS + '/bootstrap.min.css');
@@ -33,20 +33,20 @@ var fontAwesomeFonts = fontAwesomeDir + '/fonts';
 fs.copySync(fontAwesomeFonts, vendorFonts);
 console.log("Copied files from " + fontAwesomeFonts);
 
-var bootstrapJS = bootstrapDir + '/js';
-fs.copySync(bootstrapJS + '/bootstrap.min.js', vendorJS + '/bootstrap.min.js');
-console.log("Copied files from " + bootstrapJS);
+// var bootstrapJS = bootstrapDir + '/js';
+// fs.copySync(bootstrapJS + '/bootstrap.min.js', vendorJS + '/bootstrap.min.js');
+// console.log("Copied files from " + bootstrapJS);
 
 
 /*
 	Collect jquery dist files
  */
 
-var jqueryDir = './node_modules/jquery/dist';
+// var jqueryDir = './bower_components/jquery/dist';
 
-fs.copySync(jqueryDir + '/jquery.min.js', vendorJS + '/jquery.min.js');
-fs.copySync(jqueryDir + '/jquery.min.map', vendorJS + '/jquery.min.map');
-console.log("Copied files from " + jqueryDir);
+// fs.copySync(jqueryDir + '/jquery.min.js', vendorJS + '/jquery.min.js');
+// fs.copySync(jqueryDir + '/jquery.min.map', vendorJS + '/jquery.min.map');
+// console.log("Copied files from " + jqueryDir);
 
 
 /*
@@ -54,29 +54,29 @@ console.log("Copied files from " + jqueryDir);
  */
 
 
-var angularDir = './node_modules/angular';
-fs.copySync(angularDir + '/angular.min.js', vendorJS + '/angular.min.js');
-fs.copySync(angularDir + '/angular.min.js.map', vendorJS + '/angular.min.js.map');
-console.log("Copied files from " + angularDir + " to " +  vendorJS);
+// var angularDir = './bower_components/angular';
+// fs.copySync(angularDir + '/angular.min.js', vendorJS + '/angular.min.js');
+// fs.copySync(angularDir + '/angular.min.js.map', vendorJS + '/angular.min.js.map');
+// console.log("Copied files from " + angularDir + " to " +  vendorJS);
 
-var angularRouteDir = './node_modules/angular-route';
-fs.copySync(angularRouteDir + '/angular-route.min.js', vendorJS + '/angular-route.min.js');
-fs.copySync(angularRouteDir + '/angular-route.min.js.map', vendorJS + '/angular-route.min.js.map');
-console.log("Copied files from " + angularRouteDir + " to " +  vendorJS);
+// var angularRouteDir = './bower_components/angular-route';
+// fs.copySync(angularRouteDir + '/angular-route.min.js', vendorJS + '/angular-route.min.js');
+// fs.copySync(angularRouteDir + '/angular-route.min.js.map', vendorJS + '/angular-route.min.js.map');
+// console.log("Copied files from " + angularRouteDir + " to " +  vendorJS);
 
-var restangularDir = './node_modules/restangular/dist';
-fs.copySync(restangularDir + '/restangular.min.js', vendorJS + '/restangular.min.js');
-console.log("Copied files from " + restangularDir + " to " +  vendorJS);
+// var restangularDir = './bower_components/restangular/dist';
+// fs.copySync(restangularDir + '/restangular.min.js', vendorJS + '/restangular.min.js');
+// console.log("Copied files from " + restangularDir + " to " +  vendorJS);
 
-var lodashDir = './node_modules/lodash/dist';
-fs.copySync(lodashDir + '/lodash.min.js', vendorJS + '/lodash.min.js');
-console.log("Copied files from " + lodashDir + " to " +  vendorJS);
+// var lodashDir = './bower_components/lodash';
+// fs.copySync(lodashDir + '/lodash.min.js', vendorJS + '/lodash.min.js');
+// console.log("Copied files from " + lodashDir + " to " +  vendorJS);
 
 /*
 	Collect app image files
  */
 
-var assetDir = './assets';
+var assetDir = './web';
 fs.copySync(assetDir + '/images/', appImages);
 console.log("Copied files from " + assetDir + '/images/');
 
@@ -85,7 +85,7 @@ console.log("Copied files from " + assetDir + '/images/');
 	Collect app partials
  */
 
-var assetDir = './assets';
+var assetDir = './web';
 fs.copySync(assetDir + '/partials/', appPartials);
 console.log("Copied files from " + assetDir + '/partials/');
 
@@ -94,5 +94,5 @@ console.log("Copied files from " + assetDir + '/partials/');
 	TEMPORARY:  Collect app js files
  */
 
-fs.copySync(assetDir + '/js/', appJS);
-console.log("Copied files from " + assetDir + '/js/');
+// fs.copySync(assetDir + '/js/', appJS);
+// console.log("Copied files from " + assetDir + '/js/');
